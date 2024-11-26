8234527fc51a66fce94f80d4f5cc292cd7a4e342
verytouch <verytouch@qq.com>
2024-11-26 09:58:36 +0800

    改为使用gradle构建项目

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        maven {
+            url = uri("https://maven.aliyun.com/repository/public")
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "mydoc"
